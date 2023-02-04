fd452ad41a356ae9689107b73b8ac5bdc785555f
Danilo Alves <danilo_alves@acaciaconsultoria.com.br>
2023-02-04 09:48:58 -0300

    Desafio

---
+++ Aula1AspNetMVC/Aula1AspNetMVC/Controllers/AspNetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula1AspNetMVC.Controllers
+{
+    public class AspNetController : Controller
+    {
+        // GET: AspNet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
